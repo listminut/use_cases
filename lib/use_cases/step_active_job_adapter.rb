ac397a6b82b04274427a2626bd34c5efd2bfd823
Guilherme Andrade <guilherme@listminut.com>
2021-10-01 16:34:53 +0200

    fix activejob adapter

--- lib/use_cases/step_active_job_adapter.rb
+++ lib/use_cases/step_active_job_adapter.rb
@@ -14,18 +14,24 @@ module UseCases
     end
 
     def deserialize_step_arguments(args)
-      args.map { |arg| arg.is_a?(Hash) && arg.delete("_serialized_by_use_case") ? arg.deserialize : arg }
+      args.map do |arg|
+        if arg.is_a?(Hash) && arg.delete("_serialized_by_use_case")
+          arg.delete('_class').constantize.new(arg)
+        else
+          arg
+        end
+      end
     end
 
     def self.serialize_step_arguments(args)
-      args.select.with_index do |arg, index|
+      args.select.map.with_index do |arg, index|
         ActiveJob::Arguments.send(:serialize_argument, arg)
-
+        false
       rescue ActiveJob::SerializationError => _e
-        arg.serialize.merge("_serialized_by_use_case" => true)
+        args = arg.serialize.merge("_serialized_by_use_case" => true, "_class" => arg.class.name)
 
       rescue NoMethodError => _e
-        puts "[WARNING] #{arg.class} (index = #{index})" \
+        puts "[WARNING] #{arg} of class (#{arg.clas}) (index = #{index})" \
              "is not serializable and does not repond to #serialize and will be ignored."
         false
       end
